71d4f2dd7d241d690801cc6290562dc785b217c0
Belal <mohammadvns121@gmail.com>
2024-02-29 12:43:08 +0530

    Initillize project and configure sequelize

---
+++ src/index.js
@@ -0,0 +1,19 @@
+require("dotenv").config({ path: "./.env" });
+
+const sequelize = require("./db/database");
+
+const app = require("./app");
+
+sequelize
+  // .sync({ force: true })
+  .sync()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("Error Event For App !!", error);
+    });
+    console.log("Sequelise connected!");
+    app.listen(process.env.PORT || 3000, () => {
+      console.log(`Server is running at Port ${process.env.PORT || 3000}`);
+    });
+  })
+  .catch((err) => console.log("sequallize connection failed!!", err));
